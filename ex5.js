e97fdb3e05170cde331f5bc95b4290e8f0f7abd6
Natasza ASzatan <natiye.nb@gmai.com>
2023-11-08 13:39:14 +0000

    created function getAgeDifference

--- ex5.js
+++ ex5.js
@@ -1,1 +1,38 @@
 console.log('Script is working')
+
+const peopleArray = [
+    {
+        name: 'Adam',
+        age: 20
+    },
+    {
+        name: 'Amanda',
+        age: 5
+    },
+    {
+        name: 'John',
+        age: 75
+    },
+    {
+        name: 'Dave',
+        age: 15
+    }
+]
+
+console.log(getAgeDifference(peopleArray)); // 70
+
+function getYoungestPerson(peopleArray) {
+    let youngestArray = peopleArray.sort((c1, c2) => (c1.age < c2.age) ? 1 : (c1.age > c2.age) ? -1 : 0);
+    return youngestArray[0].age
+}
+
+
+
+function getOldestPerson(peopleArray) {
+    let oldestArray = peopleArray.sort((c1, c2) => (c1.age > c2.age) ? 1 : (c1.age < c2.age) ? -1 : 0);
+    return oldestArray[0].age;
+}
+
+function getAgeDifference(peopleArray) {
+    console.log(getOldestPerson(peopleArray) - getYoungestPerson(peopleArray))
+}
