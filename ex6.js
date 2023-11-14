3e42a8736aaa58cf33439f46f866dc796ed6c25d
Natasza ASzatan <natiye.nb@gmai.com>
2023-11-14 08:58:04 +0000

    first attempt

---
+++ ex6.js
@@ -0,0 +1,12 @@
+function positiveSum(arr) {
+    const arrLength = arr.length
+    const sum = 0
+    for (let i=0; i>=arr.length -1; ++i) {
+        console.log("lalalal")
+    }
+    return sum
+}
+
+
+
+console.log(positiveSum([1,2,3,4,5]))
