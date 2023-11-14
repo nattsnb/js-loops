dab12aa12b59c10e2048eddc1c27d28572ed85f7
Natasza ASzatan <natiye.nb@gmai.com>
2023-11-14 09:20:38 +0000

    finnieshed function positiveSum

--- ex6.js
+++ ex6.js
@@ -1,12 +1,10 @@
-function positiveSum(arr) {
-    const arrLength = arr.length
-    const sum = 0
-    for (let i=0; i>=arr.length -1; ++i) {
-        console.log("lalalal")
+function positiveSum(arr){
+    let sum = 0
+    for (let i=0; i<arr.length; i++) {
+        const number = arr[i]
+        if (number >= 0){
+            sum = sum + number
+        }
     }
     return sum
 }
-
-
-
-console.log(positiveSum([1,2,3,4,5]))
